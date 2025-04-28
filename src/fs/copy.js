128cbf6d683b6caeb6597d54543959d91737272d
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 20:16:53 +0300

    feat: implementation copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
+/* copy.js - implement function that copies folder files files
+   with all its content into folder files_copy at the same level
+   (if files folder doesn't exist or files_copy has already been
+   created Error with message FS operation failed must be thrown) */
+
+import * as fs from 'node:fs/promises';
+import { join } from 'node:path';
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolder = join('files');
+  const destFolder = join('files_copy');
+
+  try {
+    await fs.access(srcFolder);
+  } catch {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await fs.access(destFolder);
+
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }
+
+  await fs.mkdir(destFolder);
+
+  const srcFolderEntities = await fs.readdir(srcFolder, { withFileTypes: true });
+
+  for (const srcFolderEntity of srcFolderEntities) {
+    const srcPath = join(srcFolder, srcFolderEntity.name);
+    const destPath = join(destFolder, srcFolderEntity.name);
+
+    if (srcFolderEntity.isFile()) {
+      await fs.copyFile(srcPath, destPath);
+    }
+  }
 };
 
 await copy();
