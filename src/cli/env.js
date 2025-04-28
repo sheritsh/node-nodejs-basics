f3a56c360555bb397f0174d141b463c57bc97178
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 21:35:05 +0300

    feat: implementation env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+/* env.js - implement function that parses environment
+   variables with prefix RSS_ and prints them to the console
+   in the format RSS_name1=value1; RSS_name2=value2 */
+
 const parseEnv = () => {
-    // Write your code here 
+  const rssEnvVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(rssEnvVars);
 };
 
 parseEnv();
