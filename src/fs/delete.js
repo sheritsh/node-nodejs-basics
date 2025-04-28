4548309fb3742b7d36765505a1006094d94295d9
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 20:56:12 +0300

    feat: implementation delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+/* delete.js - implement function that deletes file
+   fileToRemove.txt (if there's no file fileToRemove.txt
+   Error with message FS operation failed must be thrown) */
+
+import * as fs from 'node:fs/promises';
+import { join } from 'node:path';
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join('files', 'fileToRemove.txt');
+
+  try {
+    await fs.unlink(filePath);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
