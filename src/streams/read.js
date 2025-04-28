a75d3b9bd9f925db672068ec808553572b3a0511
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 23:35:15 +0300

    feat: implementation read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+/* read.js - implement function that reads file fileToRead.txt content
+   using Readable Stream and prints it's content into process.stdout */
+
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join('files', 'fileToRead.txt');
+
+  const rs = createReadStream(filePath);
+
+  rs.on('error', (error) => {
+    console.error('Error reading the file:', error.message);
+  });
+
+  rs.pipe(process.stdout);
 };
 
 await read();
