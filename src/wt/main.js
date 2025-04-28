450eda66e2354eaabab645034bd4e254e6de1da4
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-29 01:25:30 +0300

    feat: implementation worker thread task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,46 @@
+/* main.js - implement function that creates number of worker threads
+(equal to the number of host machine logical CPU cores) from file worker.js
+and able to send data to those threads and to receive result of the
+computation from them. You should send incremental number starting from 10
+to each worker. For example: on host machine with 4 cores you should create
+4 workers and send 10 to first worker, 11 to second worker, 12 to third
+worker, 13 to fourth worker. After all workers will finish, function should
+log array of results into console. The results are array of objects with 2
+properties:
+ ● status - 'resolved' in case of successfully received value
+from worker or 'error' in case of error in worker
+ ● data - value from worker in case of success or null in case of
+error in worker                                                           */
+
+import { cpus } from 'node:os';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuAmount = cpus().length;
+  const workers = [];
+
+  for (let i = 0; i < cpuAmount; i++) {
+    const worker = new Worker('./worker.js');
+
+    const promise = new Promise((resolve) => {
+      worker.once('message', (data) => {
+        resolve({ status: 'resolved', data });
+      });
+
+      worker.once('error', () => {
+        resolve({ status: 'error', data: null });
+      });
+    }).finally(() => {
+      worker.terminate();
+    });
+
+    worker.postMessage(10 + i);
+    workers.push(promise);
+  }
+
+  const resArr = await Promise.all(workers);
+
+  console.log(resArr);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+/* worker.js - extend given function to work with data
+   received from main thread and implement function which
+   sends result of the computation to the main thread   */
+
+import { parentPort } from 'node:worker_threads';
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.on('message', (n) => {
+    const res = nthFibonacci(n);
+    parentPort.postMessage(res);
+  });
 };
 
 sendResult();
