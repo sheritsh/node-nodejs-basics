d961859fc2c137c878e5cbcca9258271371bf2a3
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 23:42:52 +0300

    feat: implementation write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+/* write.js - implement function that writes process.stdin data
+   into file fileToWrite.txt content using Writable Stream */
+
+import { createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join('files', 'fileToWrite.txt');
+
+  const ws = createWriteStream(filePath);
+
+  process.stdin.pipe(ws);
+
+  // press ctrl+D to finish input
+  process.stdin.on('end', () => {
+    console.log('Data has been written to file successfully');
+  });
+
+  ws.on('error', (error) => {
+    console.error('Error writing to file:', error.message);
+  });
 };
 
 await write();
