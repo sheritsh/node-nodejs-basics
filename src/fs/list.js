d7c4f079424c01da375a04ecc979693779872dab
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 21:06:28 +0300

    feat: implementation list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,29 @@
+/* list.js - implement function that prints array of all filenames
+   from files folder into console (if files folder doesn't exists Error
+   with message FS operation failed must be thrown) */
+
+import * as fs from 'node:fs/promises';
+import { join } from 'node:path';
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join('files');
+
+  try {
+    await fs.access(folderPath);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
+
+  const folderEntities = await fs.readdir(folderPath, { withFileTypes: true });
+  const filenamesArray = [];
+
+  for (const folderEntity of folderEntities) {
+    if (folderEntity.isFile()) {
+      filenamesArray.push(folderEntity.name);
+    }
+  }
+
+  console.log(filenamesArray);
 };
 
 await list();
