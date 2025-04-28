3c1ab58cb651060901b8010137b695ffe978704b
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 21:25:46 +0300

    feat: implementation read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+/* read.js - implement function that prints content of the
+   fileToRead.txt into console (if there's no file fileToRead.txt
+   Error with message FS operation failed must be thrown) */
+
+import * as fs from 'node:fs/promises';
+import { join } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join('files', 'fileToRead.txt');
+
+  try {
+    await fs.access(filePath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
+
+  const fileText = await fs.readFile(filePath, { encoding: 'utf-8' });
+
+  console.log(fileText);
 };
 
 await read();
