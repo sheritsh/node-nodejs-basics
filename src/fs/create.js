e2ca228acff60d0bd6d693aba7f09a21f120526b
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-04-28 19:52:31 +0300

    feat: implementation create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+/* create.js - implement function that creates new file fresh.txt with content
+   "I am fresh and young" inside of the files folder (if file already exists
+   Error with message FS operation failed must be thrown) */
+
+import { access, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join('files', 'fresh.txt');
+
+  try {
+    await access(filePath);
+
+    throw new Error('File already exist');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await writeFile(filePath, 'I am fresh and young');
+    } else {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await create();
